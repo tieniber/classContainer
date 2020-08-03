df8916c6d434ca02efee3ff3d464d24efac2021d
Eric Tieniber <tieniber@gmail.com>
2020-08-03 12:18:07 -0400

    v3: styles
    Added styles expression for inline styling

--- src/ClassContainer.jsx
+++ src/ClassContainer.jsx
@@ -5,11 +5,26 @@ import { hot } from "react-hot-loader/root";
 
 class ClassContainer extends Component {
     render() {
+        const dynamicClasses =
+            this.props.classes && this.props.classes.status === "available" ? this.props.classes.value : "";
+        const dynamicStyles =
+            this.props.styles && this.props.styles.status === "available" ? JSON.parse(this.props.styles.value) : {};
+
         return (
-            <div className={this.props.class + " " + this.props.classes.value} onClick={this.props.clickAction.execute}>
+            <div
+                className={this.props.class + " " + dynamicClasses}
+                style={dynamicStyles}
+                onClick={this._onclick.bind(this)}
+            >
                 {this.props.content}
             </div>
         );
+    }
+    _onclick() {
+        const { clickAction } = this.props;
+        if (clickAction && clickAction.canExecute && !clickAction.isExecuting) {
+            clickAction.execute();
+        }
     }
 }
 export default hot(ClassContainer);
